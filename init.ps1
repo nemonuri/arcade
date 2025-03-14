a2d3d99068aa444cd428e1d66d94afe03c1851ec
nemonuri <nemonuri2@gmail.com>
2025-03-14 16:12:03 +0900

    update init

--- init.ps1
+++ init.ps1
@@ -3,4 +3,12 @@ if (-not (Test-Path "$PSScriptRoot/.arcade")) {
     Import-ArcadeScript -NM -Script "ArcadeScript"    
 }
 
-. "$PSScriptRoot/.arcade/.script"
+. "$PSScriptRoot/.arcade/.script/ArcadeScript.ps1"
+
+if (-not (Test-Path "$PSScriptRoot/tool.ps1")) {
+    @'
+$ASRoot = "$PSScriptRoot/.arcade/.script"
+. "$ASRoot/ArcadeScript.ps1"
+
+'@ | Out-File -FilePath "$PSScriptRoot/tool.ps1" 
+}
