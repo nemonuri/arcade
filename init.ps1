d9e83b4f9c775da24262ae4df92a81c71e152f5f
nemonuri <nemonuri2@gmail.com>
2025-03-14 16:14:50 +0900

    change to Get-Location

--- init.ps1
+++ init.ps1
@@ -1,14 +1,14 @@
-if (-not (Test-Path "$PSScriptRoot/.arcade")) {
+if (-not (Test-Path "$(Get-Location)/.arcade")) {
     Invoke-WebRequest "https://raw.githubusercontent.com/nemonuri/arcade/master/script/ArcadeScript.ps1" | . Invoke-Expression
     Import-ArcadeScript -NM -Script "ArcadeScript"    
 }
 
-. "$PSScriptRoot/.arcade/.script/ArcadeScript.ps1"
+. "$(Get-Location)/.arcade/.script/ArcadeScript.ps1"
 
-if (-not (Test-Path "$PSScriptRoot/tool.ps1")) {
+if (-not (Test-Path "$(Get-Location)/tool.ps1")) {
     @'
 $ASRoot = "$PSScriptRoot/.arcade/.script"
 . "$ASRoot/ArcadeScript.ps1"
 
-'@ | Out-File -FilePath "$PSScriptRoot/tool.ps1" 
+'@ | Out-File -FilePath "$(Get-Location)/tool.ps1" 
 }
