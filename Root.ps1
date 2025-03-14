689897ab3891095c45074cfd7f9a3d02a8383360
nemonuri <nemonuri2@gmail.com>
2025-03-14 15:47:49 +0900

    add Root

---
+++ Root.ps1
@@ -0,0 +1,7 @@
+if (-not (Test-Path "$PSScriptRoot/.arcade")) {
+    Invoke-WebRequest "https://raw.githubusercontent.com/nemonuri/nemonuri-arcade/master/script/ArcadeScript.ps1" | . Invoke-Expression
+    Import-ArcadeScript -NM -Script "ArcadeScript"    
+}
+
+$ASRoot = "$PSScriptRoot/.arcade/.script"
+. "$ASRoot/ArcadeScript.ps1"
